598b36090edd6a80e681b573b5f7cf40cf6f92ca
Jingwen Owen Ou <jingweno@gmail.com>
2013-06-18 19:46:52 -0700

    Update to 0.7.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,9 +9,10 @@ class gh {
 
   homebrew::formula { 'gh':
     before => Package['boxen/brews/gh']
+    source => 'https://github.com/jingweno/gh/blob/master/homebrew/gh.rb'
   }
 
   package { 'boxen/brews/gh':
-    ensure => '0.6.1-boxen1'
+    ensure => '0.7.0-boxen1'
   }
 }
